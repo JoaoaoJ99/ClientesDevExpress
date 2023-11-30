904d8de7d4351e25adbbfc981b11ab1e7f1af902
joaom <joaomrodrigues114@gmail.com>
2023-11-30 21:35:52 +0000

    Add project files.

---
+++ ClientesDevExpress/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClientesDevExpress.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+        [Required]
+        public string Nome { get; set; } = "Nome";
+        [Required]
+        public string Email { get; set; } = "Email";
+        [Required]
+        [Range(900000000, 999999999)]
+        public string Telefone { get; set; } = "999999999";
+
+    }
+}
---
+++ ClientesDevExpress/Models/ClienteService.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ClientesDevExpress.Models
+{
+
+    public class ClienteService
+    {
+        public readonly string connectionString = "Server=localhost;Database=ClientesTeste;User Id=user;Password=pass;";
+
+        public async Task<List<Cliente>> GetClientesList()
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                await connection.OpenAsync();
+
+                var sql = "SELECT * FROM Clientes";
+                var clientes = await connection.QueryAsync<Cliente>(sql);
+
+                return clientes.AsList<Cliente>();
+            }
+        }
+
+        public void AddCliente(string Nome, string Email, string Telefone)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Cliente novoCliente = new Cliente {
+                    Nome = Nome,
+                    Email = Email,
+                    Telefone = Telefone
+                };
+
+                var sql = "INSERT INTO Clientes(nome, email, telefone) VALUES (@Nome, @Email, @Telefone)";
+                connection.Execute(sql, novoCliente);
+
+            }
+        }
+    }
+}
